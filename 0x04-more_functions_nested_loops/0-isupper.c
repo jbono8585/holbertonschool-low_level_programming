d4ffd40ed112bdebe050b5e5ffad1ba5934d0d8c
jbono8585 <jbono8585@gmail.com>
2020-06-21 22:13:28 +0000

    test

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,21 +4,18 @@
  *_isupper - check if C character is uppercase.
  *
  *
- *@c: is a letterthat will determine the return.
+ *@c: is a letter that will determine the return.
  *
  * Return: 1 if the letter is uppercase and 0 if lowercase.
  */
 
 int _isupper(int c);
-
 {
-int c;
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
 
-for (c = 'A'; c <= 'Z'; c++)
-{
-for (c = 'a'; c <= 'z'; c++)
-putchar(0);
-}
-putchar(1);
-return (0);
+		return (0);
 }
