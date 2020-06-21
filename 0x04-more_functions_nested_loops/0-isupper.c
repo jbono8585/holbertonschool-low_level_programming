dfb86e31c6975f98480c5d86c705e7411a773c9d
jbono8585 <jbono8585@gmail.com>
2020-06-21 15:56:06 +0000

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *_isupper - check if C character is uppercase.
+ *
+ *
+ *@c: is a letterthat will determine the return.
+ *
+ * Return: 1 if the letter is uppercase and 0 if lowercase.
+ */
+
+int _isupper(int c);
+
+{
+int c;
+
+for (c = 'A'; c <= 'Z'; c++)
+{
+for (c = 'a'; c <= 'z'; c++)
+putchar(0);
+}
+putchar(1);
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+0x04. C - More functions, more nested loops
+ Foundations - Low-level programming & Algorithm  Hatching out 
+ by Julien Barbier, co-founder at Holberton School 
+ weight: 1 
