8029c98a8554f1f4b37e78ad0611657e46d1a8be
jbono8585 <jbono8585@gmail.com>
2020-06-22 04:54:26 +0000

    test

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: length of line.
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar(10);
+}
